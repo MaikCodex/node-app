9f3af06a2278a3690a373f481cb242abaefa031d
Mikel Salvador <maiikdesiign@gmail.com>
2022-10-18 20:38:51 +0200

    ports +date

--- src/index.js
+++ src/index.js
@@ -17,6 +17,6 @@ app.use(indexRoutes)
 
 app.use(express.static(join(__dirname, 'public')))
 
-app.listen(3000)
-console.log('server is listening on port', 3000)
+app.listen(process.env.PORT || 3000)
+console.log('server is listening on port', process.env.PORT || 3000)
 
